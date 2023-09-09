3d5cd71f19908d5718a68cf2d67bdbb60a99ef45
RowBowl <robelrb@gmail.com>
2023-09-09 13:53:37 -0400

    TextField is not editable and survives recompositions. Practice using "remember" and MutableState

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -31,7 +31,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -101,11 +104,11 @@ fun TipTimeLayoutPreview() {
 
 @Composable
 fun EditNumberField(modifier: Modifier = Modifier) {
-    val amountInput: MutableState<String> = mutableStateOf("0")
-
+    //val amountInput: MutableState<String> = mutableStateOf("0")
+    var amountInput by remember { mutableStateOf("")}
     TextField(
-        value = amountInput.value,
-        onValueChange = {},
+        value = amountInput,
+        onValueChange = {amountInput = it},
         modifier = modifier
     )
 }
